6e8c15edcf88c8aae6f72131a807d2f0fe4d0c93
Aman Patel <patelpatelaman00@gmail.com>
2023-06-09 23:57:40 +0530

    .

--- src/components/common/FooterMenu.js
+++ src/components/common/FooterMenu.js
@@ -3,17 +3,9 @@ import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import { Tooltip } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
-// import SelfImprovementIcon from "@mui/icons-material/SelfImprovement";
 import Select from "../utils/Select";
 import {
-  // FOCUS_MODE,
-  // FREE_MODE,
-  // WORD_MODE_LABEL,
-  // SENTENCE_MODE_LABEL,
-  // GAME_MODE_DEFAULT,
-  // GAME_MODE_SENTENCE,
-  // TRAINER_MODE,
-  // WORDS_CARD_MODE
+ 
 } from "../../constants/Constants";
 import { Link } from "@mui/material";
 import {
@@ -22,48 +14,24 @@ import {
   GITHUB_REPO_LINK,
 } from "../../constants/Constants";
 import GitHubIcon from "@mui/icons-material/GitHub";
-// import KeyboardAltIcon from "@mui/icons-material/KeyboardAlt";
-// import EmojiFoodBeverageIcon from "@mui/icons-material/EmojiFoodBeverage";
 
-// import KeyboardAltOutlinedIcon from '@mui/icons-material/KeyboardAltOutlined';
-// import SchoolIcon from '@mui/icons-material/School';
 
 
 const FooterMenu = ({
   themesOptions,
   theme,
-  // soundMode,
+ 
   toggleSoundMode,
   handleThemeChange,
-  // toggleFocusedMode,
   toggleCoffeeMode,
   isMusicMode,
-  // isFocusedMode,
   isCoffeeMode,
   gameMode,
   handleGameModeChange,
   isTrainerMode,
   toggleTrainerMode,
-  // isWordsCardMode,
-  // toggleWordsCardMode
+ 
 }) => {
-  // const isSiteInfoDisabled = isMusicMode || isFocusedMode;
-  // const isBottomLogoEnabled = isFocusedMode && !isMusicMode;
-  // const isTypeTestEnabled = !isCoffeeMode && !isTrainerMode && !isWordsCardMode;
-
-  // const getModeButtonClassName = (mode) => {
-  //   if (mode) {
-  //     return "zen-button";
-  //   }
-  //   return "zen-button-deactive";
-  // };
-
-  // const getGameModeButtonClassName = (currMode, buttonMode) => {
-  //   if (currMode === buttonMode) {
-  //     return "active-game-mode-button";
-  //   }
-  //   return "inactive-game-mode-button";
-  // };
 
   return (
     <div className="footer">
@@ -79,101 +47,17 @@ const FooterMenu = ({
             menuPlacement="top"
           ></Select>
 
-          {/* <IconButton onClick={toggleFocusedMode}>
-            <Tooltip title={FOCUS_MODE}>
-              <span className={getModeButtonClassName(isFocusedMode)}>
-                <SelfImprovementIcon fontSize="medium"></SelfImprovementIcon>
-              </span>
-            </Tooltip>
-          </IconButton> */}
-          {/* <IconButton onClick={toggleSoundMode}>
-            <Tooltip title={SOUND_MODE_TOOLTIP}>
-              <span className={getModeButtonClassName(soundMode)}>
-                <VolumeUpIcon fontSize="medium"></VolumeUpIcon>
-              </span>
-            </Tooltip>
-          </IconButton> */}
-          {/* {soundMode && (<Select
-            classNamePrefix="Select"
-            value={soundOptions.find((e) => e.label === soundType)}
-            options={soundOptions}
-            isSearchable={false}
-            isSelected={false}
-            onChange={handleSoundTypeChange}
-            menuPlacement="top"
-          ></Select>)} */}
-          {/* <IconButton onClick={toggleWordsCardMode}>
-            <Tooltip
-              title={
-                <span style={{ whiteSpace: "pre-line" }}>{WORDS_CARD_MODE}</span>
-              }
-            >
-              <span className={getModeButtonClassName(isWordsCardMode)}>
-                <SchoolIcon fontSize="medium"></SchoolIcon>
-              </span>
-            </Tooltip>
-          </IconButton> */}
-          {/* <IconButton onClick={toggleCoffeeMode}>
-            <Tooltip
-              title={
-                <span style={{ whiteSpace: "pre-line" }}>{FREE_MODE}</span>
-              }
-            >
-              <span className={getModeButtonClassName(isCoffeeMode)}>
-                <EmojiFoodBeverageIcon fontSize="medium"></EmojiFoodBeverageIcon>
-              </span>
-            </Tooltip>
-          </IconButton> */}
-          {/* <IconButton onClick={toggleTrainerMode}>
-            <Tooltip title={TRAINER_MODE}>
-              <span className={getModeButtonClassName(isTrainerMode)}>
-                <KeyboardAltOutlinedIcon fontSize="medium"></KeyboardAltOutlinedIcon>
-              </span>
-            </Tooltip>
-          </IconButton> */}
-          {/* <IconButton onClick={toggleMusicMode}>
-            <Tooltip title={MUSIC_MODE}>
-              <span className={getModeButtonClassName(isMusicMode)}>
-                <MusicNoteIcon fontSize="medium"></MusicNoteIcon>
-              </span>
-            </Tooltip>{" "}
-          </IconButton> */}
-          {/* {isTypeTestEnabled && (
-            <>
-              <IconButton
-                onClick={() => {
-                  handleGameModeChange(GAME_MODE_DEFAULT);
-                }}
-              >
-                <span
-                  className={getGameModeButtonClassName(
-                    gameMode,
-                    GAME_MODE_DEFAULT
-                  )}
-                >
-                  {WORD_MODE_LABEL}
-                </span>
-              </IconButton>
-              <IconButton
-                onClick={() => {
-                  handleGameModeChange(GAME_MODE_SENTENCE);
-                }}
-              >
-                <span
-                  className={getGameModeButtonClassName(
-                    gameMode,
-                    GAME_MODE_SENTENCE
-                  )}
-                >
-                  {SENTENCE_MODE_LABEL}
-                </span>
-              </IconButton>
-            </>
-          )} */}
+         
+        
+       
+          
+         
+         
+        
+        
         </Box>
         { (
           <Box display="block" flexDirection="row">
-            {/* <SupportMe></SupportMe> */}
 
 
             <Tooltip
@@ -188,7 +72,7 @@ const FooterMenu = ({
                     </Link>
                     <Link
                       margin="inherit"
-                      href="https://github.com/amanpatel1527/TA-Session-Assignment" 
+                      href="https://github.com/amanpatel1527/touch_typing" 
                     >
                       {GITHUB_REPO_LINK}
                     </Link>
@@ -197,7 +81,7 @@ const FooterMenu = ({
               placement="top-start"
             >
               <IconButton
-                href="https://github.com/amanpatel1527/TA-Session-Assignment"
+                href="https://github.com/amanpatel1527/touch_typing"
                 color="inherit"
               >
                 <GitHubIcon></GitHubIcon>
